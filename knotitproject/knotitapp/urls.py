d6fcf75ba8aae5f77a05b4584d4689e499044370
MatthewScott32 <matthewscottblagg@gmail.com>
2020-06-15 10:54:13 -0500

    logout

--- knotitproject/knotitapp/urls.py
+++ knotitproject/knotitapp/urls.py
@@ -6,6 +6,7 @@ app_name = "knotitapp"
 urlpatterns = [
     path('', home, name='home'),
     path('register/', register_user, name="register"),
+    path('logout/', logout_user, name='logout'),
     path('knots/', knots_list, name='knots'),
     path('times/', times_list, name='times'),
 ]
