213e188018208e597a08652b2099afc939ac9899
MatthewScott32 <matthewscottblagg@gmail.com>
2020-06-23 15:56:40 -0500

    styled and changed knot model video name

--- knotitproject/knotitapp/views/knots/list.py
+++ knotitproject/knotitapp/views/knots/list.py
@@ -9,23 +9,10 @@ from django.contrib.auth.decorators import login_required
 def knots_list(request):
     if request.method == 'GET':
                 current_user = request.user.id
-                all_knots = Knot.objects.filter(user_id=current_user).values("id", "name", "rope_type", "company", "notes", "video" )
+                all_knots = Knot.objects.filter(user_id=current_user).values("id", "name", "rope_type", "company", "notes", "how_to_video" )
                 template = 'knots/list.html'
                 context = {
                     'all_knots': all_knots
                 }
 
                 return render(request, template, context)
-            
-    # elif request.method == 'POST':
-    #     form_data = request.POST
-    #     new_knot = Knot(
-    #         name = form_data['name'],
-    #         rope = form_data['rope_type'],
-    #         company = form_data['company'],
-    #         notes = form_data['notes'],
-    #         video = form_data['video'],
-    #         user_id = request.user.id,
-    #     ) 
-        # new_knot.save()
-        # return redirect(reverse('knotitapp:knots'))
